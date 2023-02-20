c3c8f0a00d9f4435f5675655d6e9edeb7905e321
Matthew Harris <matthew.harris.cs@gmail.com>
2023-02-20 16:20:52 -0500

    Calculating stats

--- src/app/stat-form/stat-form.component.ts
+++ src/app/stat-form/stat-form.component.ts
@@ -10,6 +10,18 @@ import { ANCESTRY_LIST, CLASS_LIST } from 'src/temp-db';
 })
 export class StatFormComponent {
 
+  /* 
+  
+  TODO:
+  Choose between key abilities for classes with them
+    Boolean to filter radio button? ngIf?
+    Rogue's Racket choice
+  Choose free stats
+    Free choice option (USE chooseBoosts)
+    Additional choice for humans OR free choose
+
+  */
+
   classes = CLASS_LIST;
   ancestries = ANCESTRY_LIST;
   classBoosts = "No class selected";
@@ -19,15 +31,77 @@ export class StatFormComponent {
   ancestrySelected = false;
   currentAncestry = null;
 
-  default = 10;
   chooseBoosts = false;
 
+  hp = 5;
   str = 10;
   dex = 10;
   con = 10;
   int = 10;
   wis = 10;
   cha = 10;
+
+  resetStats() {
+    this.str = 10;
+    this.dex = 10;
+    this.con = 10;
+    this.int = 10;
+    this.wis = 10;
+    this.cha = 10;
+  }
+
+  calculate() {
+
+    if (this.currentClass != null && this.currentAncestry != null) {
+      
+      this.resetStats();
+      // TEMP: replace with ability choice
+      let chooseKey = 'keyAbility1';
+
+      this.hp = this.currentClass['hp'] + this.currentAncestry['hp'];
+
+      this.boostStat(this.currentClass[chooseKey], true);
+      this.boostStat(this.currentAncestry['boost1'], true);
+      this.boostStat(this.currentAncestry['boost2'], true);
+      this.boostStat(this.currentAncestry['flaw'], false);
+    }
+  }
+
+  boostStat(boost: string, isBoost: boolean) {
+
+    let change = 2;
+
+    // If flaw, negate change
+    if(!isBoost) {
+      change = -2;
+    }
+
+    switch(boost) {
+      case "Strength": {
+        this.str += change;
+        break;
+      }
+      case "Dexterity": {
+        this.dex += change;
+        break;
+      }
+      case "Constitution": {
+        this.con += change;
+        break;
+      }
+      case "Intelligence": {
+        this.int += change;
+        break;
+      }
+      case "Wisdom": {
+        this.wis += change;
+        break;
+      }
+      case "Charisma": {
+        this.cha += change;
+      }
+    }
+  }
 
   showClassBoosts(name: string) {
     if (name == "---") {
@@ -38,11 +112,13 @@ export class StatFormComponent {
       let currentClass = this.classes.find((x: GameClass) => x.name == name);
       this.classBoosts = `
         ${currentClass.name}: 
+        +${currentClass.hp} HP, 
         +2 in ${currentClass.keyAbility1}`;
-      console.log(currentClass.keyAbility2);
+
       if (currentClass.keyAbility2 != null) {
         this.classBoosts = this.classBoosts.concat(` or ${currentClass.keyAbility2}`);
       }
+      
       this.classSelected = true;
       this.currentClass = currentClass;
     }
@@ -60,15 +136,17 @@ export class StatFormComponent {
         ${currentAncestry.hp} HP, 
         ${currentAncestry.size} Size, 
         Speed of ${currentAncestry.speed} ft`; 
-        if (name == "Human" || this.chooseBoosts) {
-          this.ancestryBoosts = this.ancestryBoosts.concat(`, +2 in two stats of your choice`);
-        } else {
-          this.ancestryBoosts = this.ancestryBoosts.concat(`
-            +2 in ${currentAncestry.boost1}, 
-            +2 in ${currentAncestry.boost2}, 
-            +2 in stat of your choice, and 
-            -2 in ${currentAncestry.flaw}`);
+
+      if (name == "Human" || this.chooseBoosts) {
+        this.ancestryBoosts = this.ancestryBoosts.concat(`, +2 in two stats of your choice`);
+      } else {
+        this.ancestryBoosts = this.ancestryBoosts.concat(`
+          +2 in ${currentAncestry.boost1}, 
+          +2 in ${currentAncestry.boost2}, 
+          +2 in stat of your choice, and 
+          -2 in ${currentAncestry.flaw}`);
       }
+
       this.ancestrySelected = true;
       this.currentAncestry = currentAncestry;
     }
