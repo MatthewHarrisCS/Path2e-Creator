40af2c4957f54fcf87a75285ae2681a24190d516
Matthew Harris <matthew.harris.cs@gmail.com>
2023-03-01 17:03:43 -0500

    Connection to db, template for users

--- Path2e-Backend/src/app.module.ts
+++ Path2e-Backend/src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, UserModule]
+  imports: [AuthModule, UserModule, 
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost', // FOR DEV
+      port: 3306,
+      username: 'matthewharriscs', // FOR DEV
+      password: 'path2e-pw', // FOR DEV
+      database: 'path2e_db',
+      entities: [],
+      synchronize: true
+
+  })]
 })
 export class AppModule {}
